227aa89156d2495dc347f03d64c55b79539d79cf
Damian Mediavilla <damianmediavilla@hotmail.com>
2023-10-25 14:03:25 +0200

    medicion de temperatura y humedad + trigger fan

---
+++ README.md
@@ -0,0 +1,6 @@
+# Dispositivos de FunghiFarm
+
+Este repositorio utiliza PlatformIO para el build de los programas
+
+main.cpp
+El codigo presentado utiliza el sensor DHT11 para medir temperatura y humedad.  Una humedad mayor a 60% activa el ventilador
--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,57 @@
 #include "dht_nonblocking.h"
 
 // put function declarations here:
-int myFunction(int, int);
+#define DHT_SENSOR_TYPE DHT_TYPE_11
 
-int temperaturePin = 5;
-int humidityPin = 5;
+static const int DHT_SENSOR_PIN = 2;
+int fanPin = 4;
+DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 
+static bool measure_environment( float *temperature, float *humidity )
+{
+  static unsigned long measurement_timestamp = millis( );
+
+  /* Measure once every four seconds. */
+  if( millis( ) - measurement_timestamp > 3000ul )
+  {
+    if( dht_sensor.measure( temperature, humidity ) == true )
+    {
+      measurement_timestamp = millis( );
+      return( true );
+    }
+  }
+
+  return( false );
+}
 
 void setup() {
   // put your setup code here, to run once:
   //int result = myFunction(2, 3);
+  pinMode(fanPin, OUTPUT);
   Serial.begin(9600);
 
 }
+void loop( )
+{
+  float temperature;
+  float humidity;
 
-void loop() {
-  // put your main code here, to run repeatedly:
+  /* Measure temperature and humidity.  If the functions returns
+     true, then a measurement is available. */
+  if( measure_environment( &temperature, &humidity ) == true )
+  {
+    Serial.print( "T = " );
+    Serial.print( temperature, 1 );
+    Serial.print( " deg. C, H = " );
+    Serial.print( humidity, 1 );
+    Serial.println( "%" );
+  }
+  if (humidity > 60){
+    digitalWrite(fanPin, HIGH);
+  }else{
+
+    digitalWrite(fanPin, LOW);
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
